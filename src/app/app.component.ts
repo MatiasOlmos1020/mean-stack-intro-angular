5f4a6ae6b7a60f3f9b3f03495be7914bb2db1eda
mati olmos <matiasolmosestudiorocha@gmail.com>
2022-12-30 11:43:52 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // templateUrl: './app.component.html',
+  templateUrl: './views/home.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'curso-angular2-final';
+  public titulo: string;
+
+  constructor() {
+    this.titulo = 'APP FAVORITOS TITULO';
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FavoritosListComponent } from './components/favoritos-list.component';
+
+@NgModule({
+  declarations: [
+    FavoritosListComponent,
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
